d11adb7f70177084c7afb1cfb10586064ee4eb2f
Manjit <itsmanjit20@gmail.com>
2025-10-26 20:02:11 +0530

    Update loader.py

---
+++ src/utils/loader.py
@@ -0,0 +1,16 @@
+# src/utils/loader.py
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def load_and_split_documents(path, chunk_size=1000, chunk_overlap=200):
+    loader = DirectoryLoader(path, glob="**/*.pdf", loader_cls=PyPDFLoader)
+    docs = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        add_start_index=True
+    )
+    splits = splitter.split_documents(docs)
+    return splits
