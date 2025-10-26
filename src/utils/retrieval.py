d2c63024d79045878f525f1736fbf28b4f1b3818
Manjit <itsmanjit20@gmail.com>
2025-10-26 20:37:22 +0530

    Update retrieval.py

--- src/utils/retrieval.py
+++ src/utils/retrieval.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_chroma import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 PERSIST_DIR = os.environ.get("CHROMA_PERSIST_DIR", "./chroma_db")
 EMBED_MODEL = os.environ.get("EMBEDDING_MODEL", "sentence-transformers/all-mpnet-base-v2")
@@ -18,3 +18,4 @@ def get_retriever(k=6):
     vect = load_vectorstore()
     retriever = vect.as_retriever(search_type="similarity", search_kwargs={"k": k})
     return retriever
+
