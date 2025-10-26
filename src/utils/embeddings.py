759fbe266bc2d06f01ba272d6e6415f65b9ec0f0
Manjit <itsmanjit20@gmail.com>
2025-10-26 20:01:42 +0530

    Update embeddings.py

---
+++ src/utils/embeddings.py
@@ -0,0 +1,5 @@
+# src/utils/embeddings.py
+from langchain.embeddings import HuggingFaceEmbeddings
+
+def get_embedding_model(model_name="sentence-transformers/all-mpnet-base-v2"):
+    return HuggingFaceEmbeddings(model_name=model_name)
